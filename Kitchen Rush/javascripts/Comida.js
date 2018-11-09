ecfbd5cb0c99d577de84d99bfd152896ab4e1417
Pablo M. Sanchez Trujillo <pabost23@gmail.com>
2018-11-09 22:20:00 +0100

    update juego

--- Kitchen Rush/javascripts/Comida.js
+++ Kitchen Rush/javascripts/Comida.js
@@ -1,7 +1,11 @@
-function Comida(x, y, ancho, alto, sprite) {
-    this.x = x;
-    this.y = y;
-    this.ancho = ancho;
-    this.alto = alto;
-    this.sprite = sprite;
+function Comida(ruta, idSobreZero, pos) {
+    var elementosRuta = ruta.split("/");
+    this.rutaOrigen = "./assets/images/Alimentos/" + elementosRuta[elementosRuta.length-1];
+    this.idSobreZero = idSobreZero;
+    this.idSobreUno = idSobreZero+1;
+    this.pos = pos;
+}
+
+Comida.prototype.dibujarEnCanvas = function() {
+    var canvas = document.getElementById("gameCanvas");
 }
